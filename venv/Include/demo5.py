4da388bc98351b0fce073afbfce854f80498593a
wls-admin <68422000+wls-admin@users.noreply.github.com>
2021-05-26 11:31:55 +0800

    变量

---
+++ venv/Include/demo5.py
@@ -0,0 +1,30 @@
+#使用print()  打印：hello world
+print("hello world")
+
+#使用print() 打印6 + 9的和
+print(6+9)
+
+#定义两个变量stu1和stu2，分别存储45和23，并用print()打印两个数的和
+stu1=45
+stu2=23
+print(stu1+stu2)
+
+#定义三个变量:num1,num2,num3，并用print()打印3个数的和
+num1=4
+num2=5
+num3=6
+print(num1+num2+num3)
+
+#定义一个变量num1,并赋值45.然后将num1值赋值给num2。打印出来。
+add1=45
+add2=add1
+print(add2)
+
+#定义一个变量chromeDriver存储 ”谷歌驱动”。定义变量gekodriver存储 “火狐驱动”。
+chromeDriver="谷歌驱动"
+gekodriver="火狐驱动"
+print(chromeDriver,gekodriver)
+
+#用翻译工具查询一下google、是什么意思？
+#谷歌
+
