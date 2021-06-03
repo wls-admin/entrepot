f2cf8c58106a164110877237bca584328e76e8e7
wls-admin <68422000+wls-admin@users.noreply.github.com>
2021-06-03 09:38:05 +0800

    字典练习

--- Demo3.py
+++ Demo3.py
@@ -1,3 +1,8 @@
-list=[1,2,3,4,5,6,7,8,9]
-list.reverse()
-print(list)
+dict = {"k1":"v1","k2":"v2","k3":"v3"}
+num=dict.keys()
+for i in dict:
+    print(i)
+for j in num:
+    print(dict.get(j))
+dict["k4"]="v4"
+print(dict)
