32373a0a9a713328bf744cef35b359f0474ace47
wls-admin <68422000+wls-admin@users.noreply.github.com>
2021-06-03 09:39:53 +0800

    水果商城

--- Demo5.py
+++ Demo5.py
@@ -1,8 +1,31 @@
-a=[1,5,21,30,15,9,30,24]
-b=len(a)
-h=0
-while b>=0:
-    if a[b-1]%5==0:
-        h+=a[b-1]
-    b=b-1
-print(h)
+Friuts = {
+	'苹果':12.3,  # 水果和单价
+	'草莓':4.5,
+    '香蕉':6.3,
+    '葡萄':5.8,
+    '橘子':6.4,
+    '樱桃':15.8
+}
+info = {
+    '小明': {
+        'fruits': {'苹果':4, '草莓':13, '香蕉':10},
+        'money':0
+    },
+    '小刚': {
+        'fruits': {'葡萄':19, '橘子':12, '樱桃':30},
+        'money':0
+    }
+}
+key=Friuts.keys()
+ks=info.keys()
+for i in ks:
+    s = 0
+    ity = info[i]['fruits'].keys()
+    for j in ity:
+        for ke in key:
+            h=0
+            if j==ke:
+                h=h+(info[i]['fruits'][j])*(Friuts[ke])
+                s=s+h
+    info[i]['money']=s
+print(info)
