1dfc495cae85e21024bff7d4fef461d97ea6014e
wls-admin <68422000+wls-admin@users.noreply.github.com>
2021-06-15 16:29:44 +0800

    多线程买面包

--- Demo5.py
+++ Demo5.py
@@ -1,31 +1,83 @@
-Friuts = {
-	'苹果':12.3,  # 水果和单价
-	'草莓':4.5,
-    '香蕉':6.3,
-    '葡萄':5.8,
-    '橘子':6.4,
-    '樱桃':15.8
-}
-info = {
-    '小明': {
-        'fruits': {'苹果':4, '草莓':13, '香蕉':10},
-        'money':0
-    },
-    '小刚': {
-        'fruits': {'葡萄':19, '橘子':12, '樱桃':30},
-        'money':0
-    }
-}
-key=Friuts.keys()
-ks=info.keys()
-for i in ks:
-    s = 0
-    ity = info[i]['fruits'].keys()
-    for j in ity:
-        for ke in key:
-            h=0
-            if j==ke:
-                h=h+(info[i]['fruits'][j])*(Friuts[ke])
-                s=s+h
-    info[i]['money']=s
-print(info)
+import threading
+import time
+bakery = 500
+price = 3
+c = 0
+class Person(threading.Thread):
+    username = ""
+    money = 0
+    num = 0
+
+    def run(self) -> None:
+        global bakery, price, c
+        while True:
+            if bakery > 0:
+                bakery -= 1
+                self.num += 1
+                self.money -= price
+                time.sleep(3)
+                print(self.username, "成功购买了一块面包")
+            else:
+                print(self.username, "一共抢到", self.num, "块面包，账户余额为", self.money, "元")
+                c += 1
+                break
+
+p1 = Person()
+p1.username = "张三"
+p1.money = 600
+
+p2 = Person()
+p2.username = "李四"
+p2.money = 600
+
+p3 = Person()
+p3.username = "王五"
+p3.money = 600
+
+p4 = Person()
+p4.username = "赵流"
+p4.money = 600
+
+p1.start()
+p2.start()
+p3.start()
+p4.start()
+
+class Chef(threading.Thread):
+    username = ""
+    num = 0
+    def run(self) -> None:
+        global bakery, c
+        while True:
+            if bakery >= 500:
+                time.sleep(3)
+            elif bakery < 500:
+                bakery += 1
+                self.num += 1
+                time.sleep(3)
+                print(self.username,"造了一块面包")
+            if bakery >= 500 and c==1:
+                print(self.username, "造了",self.num, "个面包")
+                break
+chef1 = Chef()
+chef1.username="66"
+
+chef2 = Chef()
+chef2.username="55"
+
+chef3 = Chef()
+chef3.username="44"
+
+chef1.start()
+chef2.start()
+chef3.start()
+
+
+
+
+
+
+
+
+
+
